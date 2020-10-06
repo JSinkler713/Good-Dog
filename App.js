40e2839bd9e8a07eb3a9ee13e3381f00ff400e44
James Sinkler <jamessinkler713@gmail.com>
2020-10-05 21:54:26 -0700

    basic landing page

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { TouchableOpacity, Image, RefreshControl, StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  const [isRefreshing, setIsRefreshing] = useState(false)
+  const [dogUri, setDogUri] = useState('https://placedog.net/300/500/s')
+
+  const handlePress = ()=> {
+    const randomNum = Math.floor(Math.random() *4)
+    const arrayOfUris = ['https://placedog.net/300/500?random/s', 'https://placedog.net/300/300?random/s', 'https://placedog.net/320/500?random/s', 'https://placedog.net/310/525/s']
+    console.log(arrayOfUris)
+    console.log(randomNum)
+    console.log(arrayOfUris[randomNum])
+    setDogUri(arrayOfUris[randomNum])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}> 
+      <Text style={styles.header}>Man's best Friend</Text>
+      <Image
+        source={{
+          width: 300,
+          height: 500,
+          uri: `${dogUri}`
+        }}
+      />
+      <TouchableOpacity onPress={handlePress} >
+        <Text>View another pooch</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
