f7b5b1c215c27a335f44d324076ea9e0de6f1f71
James Sinkler <jamessinkler713@gmail.com>
2020-10-31 16:57:25 -0700

    updates NavContainer theme, adds conditional rendering to wait for font load

--- App.js
+++ App.js
@@ -6,21 +6,40 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { asdkfljlsdf } from '@expo/vector-icons'
 import HomeScreen from './screens/HomeScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
-
+import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
 const MainStack = createStackNavigator();
 
-
+const MyTheme = {
+  dark: false,
+  fontSize: 34,
+  colors: {
+    primary: 'rgba(21, 39, 65, 0.8)',
+    background: 'rgb(178, 212, 238)',
+    card:'#829AAD',
+    text: '#BBDEFA',
+    border: 'rgb(199, 199, 204)',
+    notification: 'rgb(255, 69, 58)',
+  },
+};
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'Permanent-Marker': require('./assets/fonts/MarkerFelt.ttf'),
+  })
   //if no db table favorites set up db with favorites table with uri column
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
   return (
-    <NavigationContainer>
-      <MainStack.Navigator initialRouteName="Home">
-        <MainStack.Screen name="HomeScreen" component={HomeScreen} />
-        <MainStack.Screen name="FavoritesScreen" component={FavoritesScreen} />
+    <NavigationContainer theme={MyTheme}>
+      <MainStack.Navigator initialRouteName="All Dogs" style={styles.nav}>
+        <MainStack.Screen name="All Dogs" component={HomeScreen} />
+        <MainStack.Screen name="My Favorites" component={FavoritesScreen} />
       </MainStack.Navigator>
     </NavigationContainer>
   );
+  }
 }
 
 const styles = StyleSheet.create({
